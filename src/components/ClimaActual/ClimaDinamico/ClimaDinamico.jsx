9248fea3e39f79e9c963316adb271bd0b9891a45
Manuel <manueldonadoh@gmail.com>
2023-09-26 11:42:38 -0600

    NEW COMMIT

--- src/components/ClimaActual/ClimaDinamico/ClimaDinamico.jsx
+++ src/components/ClimaActual/ClimaDinamico/ClimaDinamico.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './ClimaDinamico.css'
 import shower from '../../../assets/images/Shower.png'
-import clear from '../../../assets/images/Clear.png'
 import HeavyCloud from '../../../assets/images/HeavyCloud.png'
 import HeavyRain from '../../../assets/images/HeavyRain.png'
 import LightCloud from '../../../assets/images/LightCloud.png'
@@ -9,6 +8,7 @@ import LightRain from '../../../assets/images/LightRain.png'
 import Sleet from '../../../assets/images/Sleet.png'
 import Snow from '../../../assets/images/Snow.png'
 import Thunderstorm from '../../../assets/images/Thunderstorm.png'
+import clear from '../../../assets/images/Clear.png'
 
 
 
--- src/components/ClimaSemana/ClimaSemanal/ClimaSemanal.jsx
+++ src/components/ClimaSemana/ClimaSemanal/ClimaSemanal.jsx
@@ -2,7 +2,6 @@ import { useState} from 'react'
 import { DateTime } from 'luxon'
 import './ClimaSemanal.css'
 import shower from '../../../assets/images/Shower.png'
-import clear from '../../../assets/images/clear.png'
 import HeavyCloud from '../../../assets/images/HeavyCloud.png'
 import HeavyRain from '../../../assets/images/HeavyRain.png'
 import LightCloud from '../../../assets/images/LightCloud.png'
@@ -10,6 +9,7 @@ import LightRain from '../../../assets/images/LightRain.png'
 import Sleet from '../../../assets/images/Sleet.png'
 import Snow from '../../../assets/images/Snow.png'
 import Thunderstorm from '../../../assets/images/Thunderstorm.png'
+import clear from '../../../assets/images/Clear.png'
 
 function ClimaSemanal({secondApi, units}) {
 
